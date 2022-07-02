3909211dc44e651342800a2da2fa6602a68ab286
Diamond Tusks <90947391+DiamondTusks@users.noreply.github.com>
2022-07-02 13:50:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+age_in_int = int(age)
+days = int(age) * 365
+weeks = int(age) * 52
+months = int(age) * 12
+
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
+
+
+
+
+
+
+
+
+
